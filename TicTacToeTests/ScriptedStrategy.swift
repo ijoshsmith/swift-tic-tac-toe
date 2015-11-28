a9e36b8448dac0af265c315598663d4581385dde
Josh Smith <josh.smith@universalmind.com>
2015-11-28 15:25:15 -0800

    remove unnecessary uppercaseString usage

--- TicTacToeTests/ScriptedStrategy.swift
+++ TicTacToeTests/ScriptedStrategy.swift
@@ -20,7 +20,7 @@ final class ScriptedStrategy {
         let uppercaseSymbols = textDiagram.characters.map { String($0).uppercaseString }
         for (index, symbol) in uppercaseSymbols.enumerate() {
             let position = GameBoard.Position(row: index / 3, column: index % 3)
-            switch symbol.uppercaseString {
+            switch symbol {
             case "X": xPositions.append(position)
             case "O": oPositions.append(position)
             default:  break
